76a6220e5da2a8726974946296d15ab4a5136eab
Eduard Urbach <e.urbach@gmail.com>
2016-02-24 16:00:16 +0900

    Set up hreflang, description and keywords meta tags

--- pages/about/about.js
+++ pages/about/about.js
@@ -6,10 +6,6 @@ With over 30 years of experience teaching the Japanese language,
 we have established ourselves with our unique teaching methods as well as the talent
 and ability of our experienced instructors.
 
-* 1
-* 2
-* 3
-
 We have a very diverse student population at our school,
 with a good majority of students from western countries.
 Our JLPT-based curriculum helps ensure a high pass-rate for any level of the test.
