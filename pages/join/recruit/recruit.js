56b671bdfba0d5693f316bff1d3e4aac55bbc0a7
Eduard Urbach <e.urbach@gmail.com>
2016-03-09 14:29:10 +0900

    Valid HTML for the recruitment page

--- pages/join/recruit/recruit.js
+++ pages/join/recruit/recruit.js
@@ -1,6 +1,15 @@
 let fs = require('fs')
 let markdownTemplate = fs.readFileSync('pages/join/recruit/recruit.md', 'utf8')
 
+// Remove IDs from the headings renderer to get valid HTML
+let marked = require('marked')
+let renderer = new marked.Renderer()
+renderer.heading = (text, level) => `<h${level}>${text}</h${level}>`
+marked.setOptions({
+	renderer
+})
+
 exports.get = (request, response) => response.render({
-	markdownTemplate
+	markdownTemplate,
+	marked
 })
