a5230d39e18d53e21e1bea157702758a7b111df5
Eduard Urbach <e.urbach@gmail.com>
2016-09-21 12:52:18 +0900

    Minor changes

--- admin/news/new/new.client.js
+++ admin/news/new/new.client.js
@@ -1,13 +1,17 @@
+let init = () => {
+	let md = window.markdownit()
+	let result = md.render('# markdown-it rulezz!')
+
+	$('preview').innerHTML = result
+}
+
 if(!window.markdownLoaded) {
 	let script = document.createElement('script')
 	script.src = 'https://cdnjs.cloudflare.com/ajax/libs/markdown-it/7.0.1/markdown-it.min.js'
-	script.onload = () => {
-		let md = window.markdownit()
-		let result = md.render('# markdown-it rulezz!')
-
-		$('preview').innerHTML = result
-	}
+	script.onload = init
 	document.head.appendChild(script)
 
 	window.markdownLoaded = true
+} else {
+	init()
 }
