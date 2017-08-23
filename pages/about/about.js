bed97d816b31a6bec9d4befc257bcc9de2f4c711
Nihongo Center Admin <admission@nihongo-center.com>
2017-08-23 13:53:36 +0900

    Update about.js

--- pages/about/about.js
+++ pages/about/about.js
@@ -5,7 +5,8 @@ let intro = {
 	ja: fs.readFileSync('pages/about/intro.ja.md', 'utf8'),
 	fr: fs.readFileSync('pages/about/intro.fr.md', 'utf8'),
 	id: fs.readFileSync('pages/about/intro.id.md', 'utf8'),
-	'zh-hant': fs.readFileSync('pages/about/intro.zh-hant.md', 'utf8')
+	'zh-hant': fs.readFileSync('pages/about/intro.zh-hant.md', 'utf8'),
+	th: fs.readFileSync('pages/about/intro.th.md', 'utf8')
 }
 
 exports.get = (request, response) => response.render({
